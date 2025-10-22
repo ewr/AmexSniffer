004783d086c4ff635ea2a792e1d9d4b73435a2b6
Eric Richardson <e@ericrichardson.com>
2025-10-21 22:03:55 -0400

    Initial Commit

---
+++ AmexSniffer/AmexSnifferApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmexSnifferApp.swift
+//  AmexSniffer
+//
+//  Created by Eric Richardson on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct AmexSnifferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmexSnifferTests/AmexSnifferTests.swift
@@ -0,0 +1,17 @@
+//
+//  AmexSnifferTests.swift
+//  AmexSnifferTests
+//
+//  Created by Eric Richardson on 10/21/25.
+//
+
+import Testing
+@testable import AmexSniffer
+
+struct AmexSnifferTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
